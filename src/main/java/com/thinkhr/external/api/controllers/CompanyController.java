2e30ebc93f1d2759f65fb06d4551e8a16af36bad
pepcus <30884555+ankurgoel30@users.noreply.github.com>
2017-11-08 05:31:19 +0000

    Sbhawsar: To sync Ankur's GetAllCompany code.

--- src/main/java/com/thinkhr/external/api/controllers/CompanyController.java
+++ src/main/java/com/thinkhr/external/api/controllers/CompanyController.java
@@ -11,11 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.thinkhr.external.api.db.entities.Company;
 import com.thinkhr.external.api.exception.APIErrorCodes;
 import com.thinkhr.external.api.exception.ApplicationException;
+import com.thinkhr.external.api.model.CompanyModel;
 import com.thinkhr.external.api.services.CompanyService;
 
 
@@ -42,8 +43,8 @@ public class CompanyController {
      * 
      */
     @RequestMapping(method=RequestMethod.GET)
-    public List<Company> getAllCompany() {
-        return companyService.getAllCompany();
+    public List<CompanyModel> getAllCompany(@RequestParam(value = "offset", required = false) Integer offset,@RequestParam(value = "limit", required = false) Integer limit,@RequestParam(value = "sort" , required = false) String sort) {
+    	return companyService.getAllCompany(offset,limit,sort);
     }
     
     /**
@@ -55,8 +56,8 @@ public class CompanyController {
      * 
      */
     @RequestMapping(method=RequestMethod.GET, value="/{companyId}")
-    public Company getById(@PathVariable(name="companyId",value = "companyId") Integer companyId) throws ApplicationException {
-        Company company = companyService.getCompany(companyId);
+    public CompanyModel getById(@PathVariable(name="companyId",value = "companyId") Integer companyId) throws ApplicationException {
+        CompanyModel company = companyService.getCompany(companyId);
         if (null == company) {
         	throw ApplicationException.createEntityNotFoundError(APIErrorCodes.ENTITY_NOT_FOUND, "company", "companyId="+ companyId);
         }
@@ -82,11 +83,11 @@ public class CompanyController {
      * @param CompanyModel object
      */
     @RequestMapping(method=RequestMethod.PUT,value="/{companyId}")
-	public ResponseEntity <Company> updateCompany(@PathVariable(name="companyId",value = "companyId") Integer companyId, 
-			@RequestBody Company company) throws ApplicationException {
-    	company.setCompanyId(companyId);
-    	companyService.updateCompany(company);
-        return new ResponseEntity<Company> (company, HttpStatus.OK);
+	public ResponseEntity <CompanyModel> updateCompany(@PathVariable(name="companyId",value = "companyId") Integer companyId, 
+			@RequestBody CompanyModel companyModel) throws ApplicationException {
+    	companyModel.setCompanyId(companyId);
+    	CompanyModel company = companyService.updateCompany(companyModel);
+        return new ResponseEntity<CompanyModel> (company, HttpStatus.OK);
 
 	}
     
@@ -97,9 +98,9 @@ public class CompanyController {
      * @param Company object
      */
     @RequestMapping(method=RequestMethod.POST)
-   	public ResponseEntity<Company> addCompany(@Valid @RequestBody Company company) throws ApplicationException {
-    	companyService.addCompany(company);
-        return new ResponseEntity<Company>(company, HttpStatus.CREATED);
+   	public ResponseEntity<CompanyModel> addCompany(@Valid @RequestBody CompanyModel companyModel) throws ApplicationException {
+    	CompanyModel company = companyService.addCompany(companyModel);
+        return new ResponseEntity<CompanyModel>(company, HttpStatus.CREATED);
    	}
 }
 
--- src/main/java/com/thinkhr/external/api/services/CompanyService.java
+++ src/main/java/com/thinkhr/external/api/services/CompanyService.java
@@ -2,17 +2,20 @@ package com.thinkhr.external.api.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.thinkhr.external.api.db.entities.Company;
 import com.thinkhr.external.api.exception.APIErrorCodes;
 import com.thinkhr.external.api.exception.ApplicationException;
+import com.thinkhr.external.api.model.CompanyModel;
 import com.thinkhr.external.api.repositories.CompanyRepository;
 
 /**
@@ -27,24 +30,25 @@ import com.thinkhr.external.api.repositories.CompanyRepository;
 */
 
 @Service
-public class CompanyService {
+public class CompanyService  extends CommonService {
 	
     @Autowired
     private CompanyRepository companyRepository;
+    public static final String DEFAULT_SORT_BY = "companyName";
 
     /**
      * Fetch all companies from database. To avoid huge number of records, we are limiting the records only upto 10 numbers.
      * 
      * @return List<Company> object 
      */
-    public List<Company> getAllCompany() {
+    public List<CompanyModel> getAllCompany(Integer offset,Integer limit ,String sortField) {
     	List<Company> companies = new ArrayList<Company>();
 
-    	//TODO: parameterize limiting of records
-    	Pageable pageble = new PageRequest(0, 10);
-    	Page<Company> companyPages  = (Page<Company>) companyRepository.findAll(pageble);
-    	companyPages.getContent().forEach(c -> companies.add(c));
-    	return companies;
+    	Pageable pageable = getPageable(offset, limit, sortField);
+    	Page<Company> companyList  = (Page<Company>) companyRepository.findAll(pageable);
+
+    	companyList.getContent().forEach(c -> companies.add(c));
+    	return companies.stream().map(c -> { return (CompanyModel) convert(c, CompanyModel.class); }).collect(Collectors.toList());
     }
     
     /**
@@ -53,33 +57,39 @@ public class CompanyService {
      * @param companyId
      * @return Company object 
      */
-    public Company getCompany(Integer companyId) {
-    	return companyRepository.findOne(companyId);
+    public CompanyModel getCompany(Integer companyId) {
+    	Company company = null;
+    	company = companyRepository.findOne(companyId);
+    	return (null != company ? (CompanyModel) convert(company, CompanyModel.class) : null);    
     }
     
     /**
      * Add a company in database
      * 
-     * @param company object
+     * @param CompanyModel object
      */
-    public Company addCompany(Company company)  {
-    	return companyRepository.save(company);
+    public CompanyModel addCompany(CompanyModel companyModel)  {
+		Company company = (Company)convert(companyModel, Company.class);
+    	companyRepository.save(company);
+    	return (CompanyModel) convert(company, CompanyModel.class);
     }
     
     /**
      * Update a company in database
      * 
-     * @param company object
+     * @param CompanyModel object
      * @throws ApplicationException 
      */
-    public Company updateCompany(Company company) throws ApplicationException  {
-    	Integer companyId = company.getCompanyId();
+    public CompanyModel updateCompany(CompanyModel companyModel) throws ApplicationException  {
+    	Integer companyId = companyModel.getCompanyId();
     	
 		if (null == companyRepository.findOne(companyId)) {
     		throw ApplicationException.createEntityNotFoundError(APIErrorCodes.ENTITY_NOT_FOUND, "company", "companyId="+companyId);
     	}
 		
-    	return companyRepository.save(company);
+		Company company = (Company)convert(companyModel, Company.class);
+    	companyRepository.save(company);
+    	return (CompanyModel) convert(company, CompanyModel.class);
 
     }
     
@@ -95,5 +105,15 @@ public class CompanyService {
     		throw ApplicationException.createEntityNotFoundError(APIErrorCodes.ENTITY_NOT_FOUND, "company", "companyId="+companyId);
     	}
     	return companyId;
+    }
+    
+    /**
+     * Return default sort field for company service
+     * 
+     * @return String 
+     */
+    @Override
+    public String getDefaultSortField()  {
+    	return null;
     }
 }
