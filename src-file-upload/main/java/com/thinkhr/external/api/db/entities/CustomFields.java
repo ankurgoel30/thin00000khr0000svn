3fe894b753394f5e35e71d2b6e8af461fca25383
pepcus <30884555+ankurgoel30@users.noreply.github.com>
2017-11-27 07:16:43 +0000

    Sbhawsar: Fixed startup error.

--- src-file-upload/main/java/com/thinkhr/external/api/db/entities/CustomFields.java
+++ src-file-upload/main/java/com/thinkhr/external/api/db/entities/CustomFields.java
@@ -1,6 +1,10 @@
 package com.thinkhr.external.api.db.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Where;
@@ -20,6 +24,9 @@ import lombok.Data;
 @Where(clause="t1_is_active=1")
 public class CustomFields {
     
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "id") 
     private Integer id;
     private Integer companyId;
     private String customFieldType;
